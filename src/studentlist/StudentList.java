f6c279f575046082b2d870334ebcd01c705c7247
elizabethdancy <elizabeth.dancy@sheridancollege.ca>
2019-05-08 13:28:53 -0400

    initial commit of tester code

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentlist;
+
+/**
+ * A class that creates an array of chars and prints
+ * them backwards
+ * @author dancye
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       char[] beads = new char[7];
+       beads[0]='l';
+    }
+    
+}
